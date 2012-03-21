a6bcc003beeeb6df25d2e8b85c91378c8196acc9
Suneil Mistry <smistry@vt.edu>
2012-03-21 18:05:16 -0400

    change to change_grade.py

--- HW4/change_grade.py
+++ HW4/change_grade.py
@@ -6,18 +6,27 @@ import os, sys
 #argument 3 is file
 #Used standard search and replace if the name is found
 
+def ch_grade(sys):
+	error = "Error: Not enough arguments"
+	if len(sys.argv) < 3:
+	    output.write(error)
+	else:
+	    string = sys.argv[1]
+	    number = sys.argv[2]
+	    input = sys.stdin
+	    output = sys.stdout
+	    if len(sys.argv) > 3:
+		input = open(sys.argv[3])
+	    if len(sys.argv) > 4:
+		output = open(sys.argv[4], 'w')
+	    for s in input.xreadlines():
+		output.write(s.replace(string, number))
+	return (string, number)
 
-if len(sys.argv) < 3:
-    print usage
-else:
-    string = sys.argv[1]
-    number = sys.argv[2]
-    input = sys.stdin
-    output = sys.stdout
-    if len(sys.argv) > 3:
-        input = open(sys.argv[3])
-    if len(sys.argv) > 4:
-        output = open(sys.argv[4], 'w')
-    for s in input.xreadlines():
-        output.write(s.replace(string, number))
-
+if __name__=='__main__':
+    from sys import stdin,stdout,stderr
+    #string = sys.argv[1]
+    #number = sys.argv[2]
+    (string,number) = ch_grade(sys)
+    stderr.write(number)
+    stderr.write(string)
